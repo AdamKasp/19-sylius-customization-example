f7a72eb0c3cd556d3156cfea2ba154ad4bbca030
Adam Kasperczak <adamkasperczak123@gmail.com>
2021-04-19 08:54:17 +0200

    initial commit

---
+++ src/Form/Extension/ProductTypeExtension.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Paweł Jędrzejewski
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace App\Form\Extension;
+
+use Sylius\Bundle\ProductBundle\Form\Type\ProductVariantType;
+use Symfony\Component\Form\AbstractTypeExtension;
+use Symfony\Component\Form\FormBuilderInterface;
+
+final class ProductTypeExtension extends AbstractTypeExtension
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder->remove('channelPricings');
+    }
+
+    public static function getExtendedTypes(): iterable
+    {
+        return [ProductVariantType::class];
+    }
+}
