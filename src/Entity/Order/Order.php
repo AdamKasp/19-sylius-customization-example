2350cb86bac3a0342a0fe7d6640958dac8d08460
Adam Kasperczak <adamkasperczak123@gmail.com>
2021-04-19 14:22:06 +0200

    [OneClickCheckout] add servis config and path config

--- src/Entity/Order/Order.php
+++ src/Entity/Order/Order.php
@@ -4,12 +4,26 @@ declare(strict_types=1);
 
 namespace App\Entity\Order;
 
+use ApiPlatform\Core\Annotation\ApiResource;
 use Doctrine\ORM\Mapping as ORM;
 use Sylius\Component\Core\Model\Order as BaseOrder;
 
 /**
  * @ORM\Entity
  * @ORM\Table(name="sylius_order")
+ *
+ * @ApiResource(
+ *      routePrefix="shop",
+ *      denormalizationContext={"groups"={"admin:order:read"}},
+ *      itemOperations={
+ *          "oneClickCheckout"={
+ *              "path"="/order/one-click-checkout",
+ *              "method"="PATCH",
+ *              "messanger"="input",
+ *              "input"="App\Command\OneClickCheckout"
+ *          }
+ *       }
+ *     )
  */
 class Order extends BaseOrder
 {
